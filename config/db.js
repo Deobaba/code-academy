1209bf09f8b4cb14353d22715dbc5de50e069c4f
Adenekan Adeola <104663055+Deobaba@users.noreply.github.com>
2023-08-30 15:41:21 +0100

    saving changes

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () =>{
-    const conn = await mongoose.connect (process.env.MONGO_URI,{ 
-        useNewUrlParser: true,useUnifiedTopology: true  })
-
-    console.log('database connected')
+const connectDB = () =>{
+    const conn =  mongoose.connect (process.env.MONGO_URI, { useUnifiedTopology: true ,  useNewUrlParser: true })
+    const db = mongoose.connection
+    db.on('connected', ()=> console.log('database connected'))
+    db.on('error', (error)=>{
+        console.log(`error occured while connecting to database ${error}`)
+    })
 }
 
 module.exports = connectDB
--- server.js
+++ server.js
@@ -77,12 +77,12 @@ app.use('/reviews', reviews);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running on port ${PORT}`.yellow.bold
   )
 );
 
